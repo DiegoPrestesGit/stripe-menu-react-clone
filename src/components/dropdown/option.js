90d96c0c22958cf5c935dabb229e929dfc58a00d
Diego Prestes <diegoprestes.git@hotmail.com>
2021-08-03 16:15:15 -0300

    feat: ensure responsiveness

--- src/components/dropdown/option.js
+++ src/components/dropdown/option.js
@@ -1,7 +1,76 @@
-import React from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
+import { motion } from 'framer-motion'
+import { useDimentions } from './dimentions'
+import { Context } from './provider'
 
-export function DropdownOption({name, content: Content}) {
+let lastOptionId = 0
+
+export function DropdownOption({name, content: Content, backgroundHeight}) {
+  const idRef = useRef(++lastOptionId)
+  const id = idRef.current
+
+  const [optionHook, optionDimentions] = useDimentions()
+  const [registered, setRegistered] = useState(false)
+
+  const {
+    registerOption,
+    updateOptionProps,
+    deleteOptionById,
+    targetId,
+    setTargetId,
+  } = useContext(Context)
+
+  useEffect(() => {
+    if (!registered && optionDimentions) {
+      const WrappedContent = () => {
+        const contentRef = useRef()
+
+        useEffect(() => {
+          const contentDimensions = contentRef.current.getBoundingClientRect()
+          updateOptionProps(id, { contentDimensions })
+        }, [])
+
+        return (
+          <div ref={contentRef}>
+            <Content />
+          </div>
+        )
+      }
+
+      registerOption({
+        id,
+        optionDimentions,
+        optionCenterX: optionDimentions.x + optionDimentions.width / 2,
+        WrappedContent,
+        backgroundHeight
+      })
+
+      setRegistered(true)
+    } else if (registered && optionDimentions) {
+      updateOptionProps(id, {
+        optionDimentions,
+        optionCenterX: optionDimentions.x + optionDimentions.width / 2
+      })
+    }
+  }, [backgroundHeight, optionDimentions, registerOption, id, registered, updateOptionProps])
+
+  const handleClick = () => {}
+  const handleTouch = () => {}
+  const handleOpen = () => {}
+  const handleClose = () => {}
+
   return (
-    <button className="dropdown-option">{name}</button>
+    <motion.button
+      className="dropdown-option"
+      ref={optionHook}
+      onMouseDown={handleClick}
+      onHoverStart={() => !window.isMobile && handleOpen()}
+      onHoverEnd={() => !window.isMobile && handleClose()}
+      onTouchStart={handleTouch}
+      onFocus={handleOpen}
+      onBlur={handleClose}
+    >
+        {name}
+    </motion.button>
   )
 }
